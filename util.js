53570cff2089a4df06f9d6c4a8b58ee62a576443
https://github.com/gitwuhao/asdfghjkl.git <https://github.com/gitwuhao/asdfghjkl.git>
2015-12-02 15:10:38 +0800

    add

--- util.js
+++ util.js
@@ -256,6 +256,45 @@ var global = window || this;
                 hash = hash & hash;
             }
             return hash;
+        },
+        eqObject: function(obj1, obj2) {
+            var result = true;
+            util.it(obj1, function(key, value) {
+                if (util.isObject(value)) {
+                    result = util.eqObject(obj1[key], obj2[key]);
+                } else if (value != obj2[key]) {
+                    result = false;
+                }
+                if (result == false) {
+                    return false;
+                }
+            });
+            return result;
+        },
+        eqArray: function(array1, array2) {
+            var defaultArray = [];
+            array1 = array1 || defaultArray;
+            array2 = array2 || defaultArray;
+            if (array1 == array2) {
+                return true;
+            } else if (array1.length != array2.length) {
+                return false;
+            }
+            var result = true;
+            util.each(array1, function(i, item) {
+                if (util.isObject(item)) {
+                    if (!util.eqObject(array1[i], array2[i])) {
+                        result = false;
+                    }
+                } else if (array1[i] != array2[i]) {
+                    result = false;
+                }
+
+                if (result == false) {
+                    return false;
+                }
+            });
+            return result;
         }
     });
 
